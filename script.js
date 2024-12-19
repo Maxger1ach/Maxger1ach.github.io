ccbbc58a2bcfcb1510b8f95a8ea14552801db23b
Maxger1ach <mgerlach0168@gmail.com>
2024-12-19 02:48:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -37,29 +37,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     document.body.style.scrollBehavior = "smooth"; // Enable smooth scrolling on mobile by default
   }
   
-// Toggle the mobile menu on hamburger click
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
-
-// Close the mobile menu when a link is clicked
-const navItems = document.querySelectorAll(".nav-links a");
-
-navItems.forEach(item => {
-  item.addEventListener("click", () => {
-    navLinks.classList.remove("active"); // This will close the menu
-  });
-});
-
-// Change header style on scroll
-window.addEventListener("scroll", () => {
-  const header = document.querySelector("header");
-  if (window.scrollY > 10) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-});
